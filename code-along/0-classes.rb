5b892e2fd1ba2c008e5654e9c641815109251381
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-02-03 00:56:56 +0000

    initial 0-classes code along

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -4,7 +4,32 @@
 # rails runner code-along/0-classes.rb
 
 # String Class
+bens_favorite_food = "sushi"
+puts bens_favorite_food.upcase
+puts bens_favorite_food.class
+
+brians_favorite_food = "tacos"
+puts brians_favorite_food.upcase
+
+number_of_tacos = 5
+puts number_of_tacos
+puts number_of_tacos.class
 
 # String Methods
 
 # Custom Class
+# can create out own classses, to create the factory:
+class Dog
+    def speak
+        puts "Woof"
+    end
+end
+
+# to create the dog
+Dog.new # gives us a new instance of the class
+jenkins = Dog.new
+puts jenkins.class
+jenkins.speak
+
+lassie = Dog.new
+lassie.speak
