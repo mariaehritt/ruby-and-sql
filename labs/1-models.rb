ac24a3b6452874f3b23baecfafbaa2683b7a76f2
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-02-03 02:14:00 +0000

    team lab 1

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -10,10 +10,34 @@ Contact.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+apple = Company.where({name: "Apple"})[0]
 
 # 2. create 1-2 new contacts for each company (they can be made up)
 
+contact = Contact.new
+contact.first_name = "Maria"
+contact.last_name = "Ehritt"
+contact.email = "e.m@gmail.com"
+contact.phone_number = "6123608930"
+contact.company_id = "1"
+contact.save
+
+contact = Contact.new
+contact.first_name = "Mary"
+contact.last_name = "Jane"
+contact.email = "m.j@gmail.com"
+contact.phone_number = "6123608931"
+contact.company_id = "2"
+contact.save
+
+puts contact.inspect
+
+
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+
+puts "Contacts: #{Contact.all.count}"
+puts "#{contact.read_attribute(:first_name)} #{contact.read_attribute(:last_name)}  - #{contact.read_attribute(:email)} "
+
 
 # ---------------------------------
 # Contacts: 4
