09abecb887e76d4cfc3052bc71f3f1b506b3750f
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-02-03 03:00:39 +0000

    group lab 2 attempt

--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -12,6 +12,15 @@
 # Elon Musk - elon@tesla.com - Tesla, Inc.
 # Tim Cook - tim@apple.com - Apple Inc.
 
+puts "Contacts: #{Contact.all.count}"
+
+apple = Company.where({ name: "Apple" })[0]
+
+contacts = apple.contacts
+for contact in contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+end
+
 # 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
 
 # ---------------------------------
@@ -24,3 +33,34 @@
 #
 # Tesla, Inc.
 # Elon Musk - elon@tesla.com
+
+# apple = Company.where({ name: "Apple" })[0]
+# amazon = Company.where({ name: "Amazon" })[0]
+# tesla = Company.where({ name: "Tesla" })[0]
+
+# contacts = apple.contacts
+# for contact in contacts
+#     puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+# end
+
+# contacts = amazon.contacts
+# for contact in contacts
+#     puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+# end
+
+# contacts = tesla.contacts
+# for contact in contacts
+#     puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+# end
+
+contacts = Contact.all
+companies = Company.all
+
+for company in companies
+    puts company.name
+    for contact in contacts
+        if contact.company.name == company.name
+            puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+        end
+    end
+end
